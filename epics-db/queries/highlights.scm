0f8d56eb4190adad49fd57a1ace1b70830ea114f
Minijackson <minijackson@riseup.net>
2022-03-25 15:45:39 +0100

    add builtin record / field types

--- epics-db/queries/highlights.scm
+++ epics-db/queries/highlights.scm
@@ -1,8 +1,11 @@
 (source_file) @local.scope
 
 (record_type) @type
+(builtin_record_type) @type.builtin
+(field_type) @type
+(builtin_field_type) @type.builtin
 (string) @string
-(string_expansion variable: (identifier) @variable)
+(string_expansion variable: (identifier) @constant)
 (string_expansion [ "${" "$(" "}" ")" ] @punctuation.bracket)
 (string_expansion "=" @punctuation.special)
 
