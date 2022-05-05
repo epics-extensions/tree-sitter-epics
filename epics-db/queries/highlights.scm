a41d783f043173bda0837c73d26f2e26fae8265b
Minijackson <minijackson@riseup.net>
2022-05-05 11:03:14 +0200

    epics-db: remove builtin types
    They are to be colorized in the client (e.g. epics.nvim)

--- epics-db/queries/highlights.scm
+++ epics-db/queries/highlights.scm
@@ -1,9 +1,7 @@
 (source_file) @local.scope
 
 (record_type) @type
-(builtin_record_type) @type.builtin
 (field_type) @type
-(builtin_field_type) @type.builtin
 (string) @string
 (escape_sequence) @string.special
 (macro_expansion) @string.special
